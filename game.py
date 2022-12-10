2e31a5650653029eab85a60046549f50a2274e01
gabriel <gabrieldennis17@gmail.com>
2022-12-10 11:13:28 +1000

    fix

--- game.py
+++ game.py
@@ -30,43 +30,41 @@ grid[10][10] = 1
 grid[11][10] = 1
 
 
-
-def draw_rect(x,y):
+def draw_rect(x, y):
     pygame.draw.rect(
-            window, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        )
+        window, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    )
+
 
 def update_board():
-    global grid 
-    global window 
+    global grid
+    global window
     window.fill(WHITE)
     for x in range(GRID_WIDTH):
         for y in range(GRID_HEIGHT):
             if grid[x][y] == 1:
-                draw_rect(x,y)
+                draw_rect(x, y)
     pygame.display.update()
 
 
 while True:
     update_board()
-    next_grid = copy.deepcopy(empty_grid) 
+    next_grid = copy.deepcopy(empty_grid)
     for x in range(GRID_WIDTH):
         for y in range(GRID_HEIGHT):
-                v = (
-                    sum(
-                        [
-                            grid[j][k]
-                            for j in range(max(x - 1,0), min(x + 2, GRID_WIDTH))
-                            for k in range(max(y - 1,0), min(y + 2, GRID_HEIGHT))
-                            if not (j == x and k == y)
-                        ]
-                    )
-                )
+            v = sum(
+                [
+                    grid[j][k]
+                    for j in range(max(x - 1, 0), min(x + 2, GRID_WIDTH))
+                    for k in range(max(y - 1, 0), min(y + 2, GRID_HEIGHT))
+                    if not (j == x and k == y)
+                ]
+            )
 
-                if grid[x][y] == 1:
-                    next_grid[x][y] = int(v in [2,3])
-                if v == 3:
-                    next_grid[x][y] = 1
+            if grid[x][y] == 1:
+                next_grid[x][y] = int(v in [2, 3])
+            if v == 3:
+                next_grid[x][y] = 1
 
     # Handle user input
     for event in pygame.event.get():
